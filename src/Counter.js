0c309db4b0f681f8cd64bcb59e112b1318648555
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-08-14 09:43:23 +0300

    add handlers and states

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
 
-function Counter(props) {
-    return (
+class Counter extends Component {
+    state = {
+      likes: 0
+    };
+  
+    addHandler = () => {
+      console.log('addHandler clicked');
+      this.setState({ likes: this.state.likes + 1 });
+    };
+  
+    removeHandler = () => {
+      console.log('removeHandler clicked');
+      this.setState({ likes: this.state.likes - 1 });
+    };
+  
+    resetHandler = () => {
+      console.log('resetHandler clicked');
+      this.setState({ likes: 0 });
+    };
+  
+    render() {
+      return (
         <div>
-            <h3>Counter will be here</h3>
+          {/* <Header /> */}
+          <div className="circle">
+          <h1 className={this.state.likes === 0 ? "likes" : this.state.likes % 2 === 0 ? "likes even" : "likes odd"}>Total likes: {this.state.likes} </h1>
+          </div>
+          <div className="buttons">
+          <button onClick={this.addHandler}>Add like</button>
+          <button onClick={this.removeHandler}>Remove like</button>
+          <button onClick={this.resetHandler}>Reset likes</button>
+          </div>
+          {/* <Footer /> */}
         </div>
-    )
-}
+      );
+    }
+  }
+  
 
 export default Counter;
